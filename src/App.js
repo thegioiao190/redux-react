15738278a494286af4ec29a6fcd50e9355a526bd
MinPink <thegioiao@gmail.com>
2021-09-22 14:50:11 +0700

    [Redux] - 19. Redux persist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import Login from "./bindings/Login"
 import './App.css';
 import {Provider} from 'react-redux'
 import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from "./redux/store";
 
 function App() {
   return (
     <Provider store={store} className="App">
-      <Login/>
+      <PersistGate loading={null} persistor={persistor}>
+        <Login/>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,26 @@ import { combineReducers} from "redux"
 import todoReducer from './todos'
 import authReducer from './auth'
 import { configureStore } from "@reduxjs/toolkit"
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:["auth"]
+  }
 
 const reducer = combineReducers({
     todo:todoReducer,
     auth:authReducer
 })
 
-export default configureStore({
-    reducer
-});
+const persistedReducer = persistReducer(persistConfig, reducer)
+ 
+const store = configureStore({
+    reducer:persistedReducer
+})
+
+export const persistor = persistStore(store)
+
+export default store;
