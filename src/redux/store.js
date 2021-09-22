d4511d1a58d3a32de58281ebf75bec09353e07f0
MinPink <thegioiao@gmail.com>
2021-09-22 14:28:47 +0700

    [Redux] - 17. Redux toolkit configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,11 @@
-import { createStore, combineReducers, applyMiddleware} from "redux"
+import { combineReducers} from "redux"
 import todoReducer from './todos'
-import thunk from "redux-thunk"
+import { configureStore } from "@reduxjs/toolkit"
 
 const reducer = combineReducers({
     todo:todoReducer
 })
 
-const myMiddleware = store => next => action => {
-    if (action.type === "ADD_TODO" && action.payload === "fuck"){
-        action.payload = "f***";
-    }
-    return next(action)
-}
-
-// const asyncMiddleware = store => next => action => {
-//     console.log(next)
-//     if(typeof(action)==="function"){
-//         return action(next);
-//     }
-//     return next(action)
-// }
-
-export default createStore(reducer,applyMiddleware(myMiddleware,thunk));
+export default configureStore({
+    reducer
+});
